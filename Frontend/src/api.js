67e990563ab5f284b8770f7634c1bb89ec4cf253
Samarth Agarwal <144302485+samarth9685@users.noreply.github.com>
2025-07-28 09:40:14 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, // your backend URL
+  baseURL: process.env.VITE_API_URL, // your backend URL
 });
 
 API.interceptors.request.use((req) => {
